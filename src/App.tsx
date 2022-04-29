55ff1286567185e67b3055ccd981c5433ba8e952
nabcellent <nabcellent.dev@gmail.com>
2022-04-29 09:40:34 +0300

    Implemented sign in with otp verification
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { auth, RecaptchaVerifier, signInWithPhoneNumber } from './firebase';
+import { useEffect } from 'react';
+import { ApplicationVerifier } from '@firebase/auth';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    let appVerifier: ApplicationVerifier;
+    useEffect(() => {
+        appVerifier = new RecaptchaVerifier('recaptcha-container', {
+            size: 'invisible',
+            defaultCountry: 'KE'
+        }, auth);
+    });
+
+    const handleClick = () => {
+        let number = '+254110039317';
+
+        signInWithPhoneNumber(auth, number, appVerifier).then(function (res) {
+            let code = prompt('Enter the otp', '');
+
+            if (code === null) return;
+
+            res.confirm(code).then(result => {
+                console.log(result.user);
+
+                document.querySelector('label')!.textContent = result.user?.phoneNumber + " Number verified";
+            }).catch(err => console.log(err));
+        });
+    };
+
+    return (
+        <div className="App">
+            <div id="recaptcha-container"></div>
+
+            <label htmlFor="test"></label>
+
+            <button onClick={handleClick}>Click here</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyArNiQJyg6PgXnZsesyKs4tsckY2NufjFk",
+    authDomain: "fluent-imprint-335817.firebaseapp.com",
+    projectId: "fluent-imprint-335817",
+    storageBucket: "fluent-imprint-335817.appspot.com",
+    messagingSenderId: "894866721271",
+    appId: "1:894866721271:web:2d14d2fb5528f2592806f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth, RecaptchaVerifier, signInWithPhoneNumber };
